ebd9938850abcb02a685339b43e96398253c4a25
ry <2351633417@qq.com>
2018-12-29 14:17:58 +0800

    修改了controller 2018.12.29  14：17提交

--- sunshine-buyer/src/main/java/renying/controller/LoginController.java
+++ sunshine-buyer/src/main/java/renying/controller/LoginController.java
@@ -1,10 +1,11 @@
 package renying.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.entity.Buyer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,11 +19,14 @@ public class LoginController {
 	private BuyerService buyerService;
 	
 	@RequestMapping("login")
-	public Buyer login(@RequestParam("buyerName")String buyerName,@RequestParam("buyerPassword")String buyerPassword) {
-		if(buyerService.login(buyerName,buyerPassword)!=null) {
-			return buyerService.login(buyerName,buyerPassword);
+	public Buyer login(@RequestParam("buyerName")String buyerName,@RequestParam("buyerPassword")String buyerPassword,
+						HttpSession session) {
+		Buyer buyer=buyerService.login(buyerName, buyerPassword);
+		if(buyer!=null) {
+			session.setAttribute("buyerSession",buyer );
+			return buyer;
 		}
-		return buyerService.login(buyerName,buyerPassword)  ;
+		return null;
 		
 	}
 	
